17d45bc4edf2c3321678f2cc8012ed69a5bd217d
jfzhou90 <burninghot4jesus2005@gmail.com>
2018-06-22 13:47:01 -0400

    auth routes updated

--- server/index.js
+++ server/index.js
@@ -3,3 +3,20 @@ const cookieSession = require('cookie-session')
 const passport = require('passport')
 const keys = require('./config/keys')
 const bodyParser = require('body-parser');
+
+const app = express();
+
+app.use(
+    cookieSession({
+      maxAge: 30 * 24 * 60 * 60 * 1000,
+      keys: [keys.cookieKey]
+    })
+)
+app.use(bodyParser.json());
+app.use(passport.initialize())
+app.use(passport.session())
+
+const PORT = process.env.PORT || 5000
+app.listen(PORT)
+
+
