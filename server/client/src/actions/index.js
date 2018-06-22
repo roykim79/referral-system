5acfba4143f4a5144257fd8e9324bf9cdfcd24e3
Altneuj <34726282+Altneuj@users.noreply.github.com>
2018-06-22 17:57:26 -0400

    added fetch tags action, reducer

--- server/client/src/actions/index.js
+++ server/client/src/actions/index.js
@@ -1,15 +1,21 @@
 import axios from 'axios';
-import { FETCH_USER, FETCH_REFERRALS } from './types';
+import { FETCH_USER, FETCH_REFERRALS, FETCH_TAGS } from './types';
 const ROOT_URL = 'http://localhost:5000'
 
 export const fetchUser = () => {
-    const response = axios.get(`${ROOT_URL}/api/current_user`);
+    let response = axios.get(`${ROOT_URL}/api/current_user`);
 
     return {type: FETCH_USER, payload: response}
 }
 
 export const fetchReferrals = () => {
-    const response = axios.get(`${ROOT_URL}/api/referrals`);
+    let response = axios.get(`${ROOT_URL}/api/referrals`);
 
     return {type: FETCH_REFERRALS, payload: response}
+}
+
+export const fetchTags = () => {
+    let response = axios.get(`${ROOT_URL}/api/tags`);
+
+    return {type: FETCH_TAGS, payload: response}
 }
