cff37b0464b9ed566485b9d13bbede7fff60a7e6
jfzhou90 <burninghot4jesus2005@gmail.com>
2018-06-22 16:19:58 -0400

    updated

--- server/index.js
+++ server/index.js
@@ -1,37 +1,36 @@
-const express = require('express');
-const cookieSession = require('cookie-session');
-const passport = require('passport');
-const keys = require('./config/keys');
+const express = require('express')
+const mongoose = require('mongoose')
+const cookieSession = require('cookie-session')
+const passport = require('passport')
+const keys = require('./config/keys')
 const bodyParser = require('body-parser');
 
-const app = express();
+mongoose.connect(keys.mongoURI)
 
-const sequelize = new Sequelize('database', 'username', 'password', {
-    host: 'localhost',
-    dialect: 'mysql'|'mariadb'|'sqlite'|'postgres'|'mssql',   //choose anyone between them
-  
-    // To create a pool of connections
-    pool: {
-      max: 5,
-      min: 0,
-      idle: 10000
-    },
-  
-    // For SQLite only
-    storage: 'path/to/database.sqlite'
-  });
+const app = express()
+app.use(bodyParser.json());
 
 app.use(
-    cookieSession({
-      maxAge: 30 * 24 * 60 * 60 * 1000,
-      keys: [keys.cookieKey]
-    })
+  cookieSession({
+    maxAge: 30 * 24 * 60 * 60 * 1000,
+    keys: [keys.cookieKey]
+  })
 )
-app.use(bodyParser.json());
 app.use(passport.initialize())
 app.use(passport.session())
 
+if (process.env.NODE_ENV === 'production') {
+  // Express will serve up production assets
+  // like our main.js file, or main.css file!
+  app.use(express.static('client/build'));
+
+  // Express will serve up the index.html file
+  // if it doesn't recognize the route
+  const path = require('path');
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+  });
+}
+
 const PORT = process.env.PORT || 5000
 app.listen(PORT)
-
-
