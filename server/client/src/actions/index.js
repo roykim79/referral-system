dc990648902e9d856e6d86029470bc0769a794e3
Altneuj <34726282+Altneuj@users.noreply.github.com>
2018-06-22 22:11:01 -0400

    added fetch-org actions need work

--- server/client/src/actions/index.js
+++ server/client/src/actions/index.js
@@ -1,5 +1,5 @@
 import axios from 'axios';
-import { FETCH_USER, FETCH_REFERRALS, FETCH_TAGS } from './types';
+import { FETCH_USER, FETCH_REFERRALS, FETCH_TAGS, FETCH_ORGANIZATIONS } from './types';
 const ROOT_URL = 'http://localhost:5000'
 
 export const fetchUser = () => {
@@ -18,4 +18,17 @@ export const fetchTags = () => {
     let response = axios.get(`${ROOT_URL}/api/tags`);
 
     return {type: FETCH_TAGS, payload: response}
+}
+export const fetchOrganizations = (tag, page) =>{
+    let BASE_URL = `${ROOT_URL}/api/organizations?`;
+    if(tag) {
+        BASE_URL.concat(`tag=${tag}`);
+    } 
+    if (page) {
+        BASE_URL.concat(`page=${page}`);
+    }
+
+    let response = axios.get(BASE_URL)
+
+    return {type: FETCH_ORGANIZATIONS, payload: response};
 }
--- server/client/src/actions/types/index.js
+++ server/client/src/actions/types/index.js
@@ -1,3 +1,4 @@
 export const FETCH_USER = 'fetch_user';
 export const FETCH_REFERRALS = 'fetch_referrals';
 export const FETCH_TAGS = 'fetch_tags';
+export const FETCH_ORGANIZATIONS = 'fetch_organizations';
