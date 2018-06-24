77233a6f87ae98b4fb2b967016c5f7ae30cd2947
jfzhou90 <burninghot4jesus2005@gmail.com>
2018-06-24 17:12:20 -0400

    update schema

--- server/middlewares/requireLogin.js
+++ server/middlewares/requireLogin.js
@@ -2,7 +2,6 @@ module.exports = (req, res, next) => {
     if (!req.user) {
         return res.status(401).redirect('/landing');
     }
-    
     req.user.lastActive = new Date();
     next();
   };
