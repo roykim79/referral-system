909fe0c964758e8c195b67d74d64831ccf1734f3
Roy Kim <rykim79@gmail.com>
2018-06-25 15:21:49 -0400

    added html for header

--- server/client/src/components/Header.js
+++ server/client/src/components/Header.js
@@ -1,15 +1,14 @@
 import React, { Component } from 'react';
 import axios from 'axios';
-import {ROOT_URL} from '../actions'
 
 class Header extends Component {
   // constructor() {
 
   // }
 
-  logOut() {
-    axios.get(`${ROOT_URL}/api/logout`);
-  }
+  // logOut() {
+  //   axios.get(`/api/logout`);
+  // }
 
   render() {
     // if (!userLoggedIn) {
@@ -22,8 +21,8 @@ class Header extends Component {
     return (
       <div className="header">
         <h1>Referral System</h1>
-        <button onClick={this.logOut}>Logout</button>
-        {/* <button className="header-logout">Logout</button> */}
+        {/* <button onClick={this.logOut}>Logout</button> */}
+        <button className="header-logout">Logout</button>
       </div>
     )
   }
