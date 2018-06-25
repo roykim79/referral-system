50d2da2a0942df9797f0280c95541c8b5b3b8d56
Altneuj <34726282+Altneuj@users.noreply.github.com>
2018-06-25 12:24:36 -0400

    react-table cooperating linking row

--- server/client/src/components/ReferralList.js
+++ server/client/src/components/ReferralList.js
@@ -1,16 +1,42 @@
 import React, {Component} from 'react';
+import ReactTable from 'react-table';
+import {connect} from 'react-redux';
+import {Link, withRouter} from 'react-router-dom';
 
 class ReferralList extends Component {
     constructor(props) {
         super(props)
     }
+    renderTable = () => {
+        let fakeData = [{status: 'pending', id:'landing', referring_organization : {organizationName: 'testOrg'}}]
+        let columns = [{Header: 'Organization', accessor: 'referring_organization.organizationName'},{Header: 'status', accessor: 'status'}]
+        let data;
+        if(this.props.status !== null){
+            data = fakeData.filter((referral) => {
+                return referral.status == this.props.status;
+            })
+        } else {
+            data = fakeData;
+        }
+        return <ReactTable 
+                    getTrProps={(state, rowInfo, column) => {
+                        return {
+                            onClick: (e) => {
+                                this.props.history.push(`/${rowInfo.original.id}`)
+                            }
+                        }
+                    }}
+                    data={data}
+                    columns={columns}
+        />
+    }
+    
     render() {
-        return(
-            <div>
-                <h6>This is the list of the referrals that will be filtered</h6>
-            </div>
-        )
+        return this.renderTable()
     }
 }
 
-export default ReferralList; 
+const mapStateToProps = ({referrals}) => {
+    return {referrals}
+}
+export default connect(mapStateToProps)(withRouter(ReferralList)); 
