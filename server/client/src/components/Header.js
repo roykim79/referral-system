a8d2ce1d15fe3d1aa317e89e32622da32f5f7c3b
Roy Kim <rykim79@gmail.com>
2018-06-25 15:09:18 -0400

    created header component, still need userLoggedIn logic

--- server/client/src/components/Header.js
+++ server/client/src/components/Header.js
@@ -1,13 +1,32 @@
-import React, {Component} from 'react';
+import React, { Component } from 'react';
+import axios from 'axios';
+import {ROOT_URL} from '../actions'
 
 class Header extends Component {
-    render(){
-        return (
-            <div>
-                <h2>This is the header component</h2>
-            </div>
-        )
-    }
+  // constructor() {
+
+  // }
+
+  logOut() {
+    axios.get(`${ROOT_URL}/api/logout`);
+  }
+
+  render() {
+    // if (!userLoggedIn) {
+    //   return (
+    //     <div className="header">
+    //       <h1>Referral System</h1>
+    //     </div>
+    //   )
+    // }
+    return (
+      <div className="header">
+        <h1>Referral System</h1>
+        <button onClick={this.logOut}>Logout</button>
+        {/* <button className="header-logout">Logout</button> */}
+      </div>
+    )
+  }
 }
 
 export default Header;
