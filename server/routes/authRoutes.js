a600bf2d4b56f4a4eb8a8a0bdcb6f7ee51aa1aae
jfzhou90 <burninghot4jesus2005@gmail.com>
2018-06-26 10:35:24 -0400

    login

--- server/routes/authRoutes.js
+++ server/routes/authRoutes.js
@@ -3,10 +3,7 @@ const User = require('../models/User')
 
 module.exports = app => {
   // if login success redirect to /dashboard, if fail redirect to /landing
-  app.post('/api/login', passport.authenticate('local', { 
-    successRedirect: '/dashboard',
-    failureRedirect: '/'
-  }));
+  app.post('/api/login', passport.authenticate('login', { successFlash: 'Welcome!' }));
 
 
   // just to check if cookie is working.
