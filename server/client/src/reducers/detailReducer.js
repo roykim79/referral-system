2b5e25b649994daccb97e1503d45ee45162f1c01
Matthew Keller <matthewkeller@Hews-MacBook-Air.local>
2018-06-27 14:34:22 -0400

    added more JSX and added an action to empty redux details

--- server/client/src/reducers/detailReducer.js
+++ server/client/src/reducers/detailReducer.js
@@ -1,13 +1,17 @@
-import { SUBMIT_NOTE, FETCH_DETAIL } from "../actions/types";
+import { SUBMIT_NOTE, FETCH_DETAIL, EMPTY_DETAIL } from "../actions/types";
 
 export default function(state = null, action) {
     switch (action.type) {
         case FETCH_DETAIL:
 
             return action.payload.data || null;
-        case SUBMIT_NOTE: 
+        case SUBMIT_NOTE:
 
             return action.payload.data || null;
+
+        case EMPTY_DETAIL:
+
+            return null;
         default:
         return state;
     }
