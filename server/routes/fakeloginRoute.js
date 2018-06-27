9695e9e0076fdfda3d2e1599ccf2b9cee0fa93a3
jfzhou90 <burninghot4jesus2005@gmail.com>
2018-06-27 15:35:19 -0400

    test

--- server/routes/fakeloginRoute.js
+++ server/routes/fakeloginRoute.js
@@ -38,7 +38,7 @@ module.exports = app => {
                 tags: tagIds,
                 logo: "https://99designs-start-attachments.imgix.net/alchemy-pictures/2016%2F02%2F22%2F04%2F24%2F31%2Fb7bd820a-ecc0-4170-8f4e-3db2e73b0f4a%2F550250_artsigma.png?auto=format&ch=Width%2CDPR&w=250&h=250"
             })
-    
+      //test
             let user = new Users({
                 username: userNameArray[i],
                 firstName: "Aaron",
