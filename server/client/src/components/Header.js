5cae2ce4bcb24f7b7dc1addfd4e6c96ef08b78cb
Roy Kim <rykim79@gmail.com>
2018-06-28 12:03:29 -0400

    conflict fix

--- server/client/src/components/Header.js
+++ server/client/src/components/Header.js
@@ -26,13 +26,9 @@ class OtherHeader extends React.Component {
       <header className="mdc-top-app-bar mdc-top-app-bar--short app-nav-header ">
         <div className="mdc-top-app-bar__row">
           <section className="mdc-top-app-bar__section mdc-top-app-bar__section--align-start">
-<<<<<<< HEAD
-            <a href="/"><img className="shrine-logo-drawer ml-1" src={logo}/></a>
-=======
             <a href="/">
               <img className="shrine-logo-drawer ml-1" src={logo}/>
             </a>
->>>>>>> 5dc03ace055d93d23d1260517cf152d0ee9b4459
           </section>
           <section className="mdc-top-app-bar__section mdc-top-app-bar__section--align-end app-nav-header" role="toolbar">
             <a href="#" className="mdc-top-app-bar__action-item action-margin">My Organization</a>
