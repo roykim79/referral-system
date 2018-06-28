5d551e214b8e4cddf305a97d1e55291fd258cfdb
Altneuj <34726282+Altneuj@users.noreply.github.com>
2018-06-28 12:45:14 -0400

    fixing org info

--- server/client/src/components/Header.js
+++ server/client/src/components/Header.js
@@ -28,7 +28,7 @@ class OtherHeader extends React.Component {
             <img className="shrine-logo-drawer ml-1" src={logo}/>
           </section>
           <section className="mdc-top-app-bar__section mdc-top-app-bar__section--align-end app-nav-header" role="toolbar">
-            <a href="#" className="mdc-top-app-bar__action-item action-margin">My Organization</a>
+            <a href="/my-org" className="mdc-top-app-bar__action-item action-margin">My Organization</a>
 
             <a onClick={this.logOut} className="mdc-top-app-bar__action-item action-margin">Logout</a>
           </section>
